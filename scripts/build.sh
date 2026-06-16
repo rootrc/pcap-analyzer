78583b78ef0c5f49d46cca6b914e2c3ac6d9489c
rootrc <rootrc123@gmail.com>
2026-06-16 01:17:54 -0400

    Change build script to not build testing files

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
-cmake -S "$PROJECT_ROOT" -B "$PROJECT_ROOT/build"
+cmake -S "$PROJECT_ROOT" -B "$PROJECT_ROOT/build" -DBUILD_TESTING=OFF
 cmake --build "$PROJECT_ROOT/build"
